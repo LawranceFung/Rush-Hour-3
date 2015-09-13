231201c2cfa94a471d038bc45bf3bb5fc955d636
JarianC <cottinghamja@gmail.com>
2015-09-13 01:38:27 -0500

    ux push, waiting for david

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-	<style>    
+	<style>
 	body {padding-top: 50px;}
 	.day {padding-left: 100px;}
 	.hours {padding-left: 100px;}
@@ -11,7 +11,7 @@
 	</style>
 	<link rel="stylesheet" type="text/css" href="index.css">
 </head>
-<body>        
+<body>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script>
 		function slugify(text) {
@@ -21,24 +21,24 @@
 				.replace(/\-\-+/g, '-') // Replace multiple - with single -
 				.replace(/^-+/, '') // Trim - from start of text
 				.replace(/-+$/, ''); // Trim - from end of text
-		}	
+		}
 		// REsponse to clicking the submit button
 		$(document).ready(function(){
-			$("#submit").click(function(event){	   
-		    	//Cohere address	    	
-		    	var address = document.getElementById("address").value;		    	
+			$("#submit").click(function(event){
+		    	//Cohere address
+		    	var address = document.getElementById("address").value;
 		    	var jurisdictionString = "Court with jurisdiction over violations at: ";
 		    	document.getElementById('violationAddress').innerHTML = jurisdictionString.concat(address);
-		    	
+
 		    	// Cohere API call
 				var apiKey = 'AIzaSyC0GUd7gFoVVt03p9-hR1dLfsjmrUm9pTM';
-				var apiBase = 'https://maps.googleapis.com/maps/api/geocode/json?address=';				
+				var apiBase = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
 				var baseAddress = apiBase.concat(slugify(address));
 				var baseKey = baseAddress.concat("&key=").concat(apiKey);
 				var data;
-				$.getJSON(baseKey, function(data) {				    				    
+				$.getJSON(baseKey, function(data) {
 				    var xy = {"x": JSON.stringify(data['results'][0]['geometry']['location']['lat']), "y":JSON.stringify(data['results'][0]['geometry']['location']['lng'])};
-				    
+
 					var destUrl = 'http://www.ayeke.me:3000/municipal/';
 					var sendXYUrl = destUrl.concat(JSON.stringify(data['results'][0]['geometry']['location']['lat'])).concat("/").concat(JSON.stringify(data['results'][0]['geometry']['location']['lng']));
 					// $.get(sendXYUrl);
@@ -48,31 +48,31 @@
 				        dataType:"json",
 				        json:"mycallback",
 				        success:function(data2)
-				        {				            
-				       		document.getElementById('temp').innerHTML = JSON.stringify(data2);     
+				        {
+				       		document.getElementById('temp').innerHTML = JSON.stringify(data2);
 				       		// Put JSON data into divs
 				       		// Court
-				       		document.getElementById('courtName').innerHTML = JSON.stringify(data2['municipality']['name']);     
-				       		document.getElementById('courtName').setAttribute('href', JSON.stringify(data2['municipality']['municipal_court_website']));
+				       		document.getElementById('courtName').innerHTML = JSON.stringify(data2['municipality']['name']);
+				       		document.getElementById('siteLink').setAttribute('href', JSON.stringify(data2['municipality']['municipal_court_website']));
 				       		var courtAddress = "Address: ";
-				       		document.getElementById('courtAddress').innerHTML = courtInfo.concat(JSON.stringify(data2['address']), ", ", JSON.stringify(data2['city']), ", ", JSON.stringify(data2['state']), ", ", JSON.stringify(data2['zip_code']));   
-				       		var plus = "+";	
+				       		document.getElementById('courtAddress').innerHTML = courtInfo.concat(JSON.stringify(data2['address']), ", \n", JSON.stringify(data2['city']), ", ", JSON.stringify(data2['state']), ", ", JSON.stringify(data2['zip_code']));
+				       		var plus = "+";
 				       		var courtAddress = "Address: "
-				       		document.getElementById('courtPhone').innerHTML = courtAddress.concat(JSON.stringify(data2['phone']));     
-				       		document.getElementById('courtPhone').setAttribute('href', plus.concat(slugify(JSON.stringify(data2['phone']))));  
-				       		// document.getElementById('courtInfo').innerHTML = courtInfo.concat(JSON.stringify(data2['municipality']['court_docket_listed']), ", ", );     
+				       		document.getElementById('courtPhone').innerHTML = courtAddress.concat(JSON.stringify(data2['phone']));
+				       		document.getElementById('courtPhone').setAttribute('href', plus.concat(slugify(JSON.stringify(data2['phone']))));
+				       		// document.getElementById('courtInfo').innerHTML = courtInfo.concat(JSON.stringify(data2['municipality']['court_docket_listed']), ", ", );
 				        }
 				    });
 					// document.getElementById('temp').innerHTML = sendXYUrl;
 					// var xmlHttp = new XMLHttpRequest();
-				 //    xmlHttp.onreadystatechange = function() { 
+				 //    xmlHttp.onreadystatechange = function() {
 				 //        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
 				 //            var temp = callback(xmlHttp.responseText);
 				 //            document.getElementById('temp').innerHTML = temp;
 				 //            // Put the JSON data into the page
 				 //        }
 				 //    }
-				 //    xmlHttp.open("GET", sendXYUrl, true); // true for asynchronous 
+				 //    xmlHttp.open("GET", sendXYUrl, true); // true for asynchronous
 				 //    console.log();
 				 //    xmlHttp.send(null);
 				});
@@ -81,19 +81,19 @@
 		    	document.getElementById('popup').style.visibility = 'visible';
 		    	event.preventDefault();
 		    });
-		});		
+		});
 		</script>
 		Live in Saint Louis County?  Find where to challenge a ticket
 		<!-- Input Form -->
 		<form method="POST">
 			Where did you get your ticket? <br>
-			<input type="text" name="address" id="address">	  
-			<br>	
+			<input type="text" name="address" id="address">
+			<br>
 			<input type="submit" id="submit" value="Submit">
 		</form>
 
 		<!-- Popup that shows processed data -->
-		<div class="hide" id="popup">						
+		<div class="hide" id="popup">
 			<b id="violationAddress"></b>
 			<br>
 			<div id="courtName"></div>
